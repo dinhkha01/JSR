bd59b1538c3c147a4cc1b3e1a9f23ae9948333e4
dinhkha01 <149988948+dinhkha01@users.noreply.github.com>
2024-05-14 08:42:12 +0700

    a

--- ss1/bai11.js
+++ ss1/bai11.js
@@ -5,22 +5,22 @@ let gioHang = [
   { id: 4, name: "dép", quantity: 1, price: 1500000 },
 ];
 
-function thayDoiSoLuong(idSanPham, soLuongMoi, mangSanPham) {
-  let sanPham = mangSanPham.find((sp) => sp.id === idSanPham);
+function thayDoiSoLuong(idSanPham, soLuongMoi, giahang) {
+  let sanPham = giahang.find((sp) => sp.id === idSanPham);
   if (sanPham) {
     sanPham.quantity = soLuongMoi;
   }
 }
 
-function xoaSanPham(idSanPham, mangSanPham) {
-  let viTri = mangSanPham.findIndex((sp) => sp.id === idSanPham);
+function xoaSanPham(idSanPham, giahang) {
+  let viTri = giahang.findIndex((sp) => sp.id === idSanPham);
   if (viTri !== -1) {
-    mangSanPham.splice(viTri, 1);
+    giahang.splice(viTri, 1);
   }
 }
 
-function tinhTongGia(mangSanPham) {
-  return mangSanPham.reduce((sum, sp) => sum + sp.price * sp.quantity, 0);
+function tinhTongGia(giahang) {
+  return giahang.reduce((sum, sp) => sum + sp.price * sp.quantity, 0);
 }
 
 thayDoiSoLuong(1, 3, gioHang);
