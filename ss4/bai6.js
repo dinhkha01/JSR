f622051c06c5b70ec9ba9c0bb84e8b0cfe4297ce
dinhkha01 <149988948+dinhkha01@users.noreply.github.com>
2024-05-15 16:10:20 +0700

    a

---
+++ ss4/bai6.js
@@ -0,0 +1,8 @@
+let checkEndString=(str,length)=>{
+        let check=str.endsWith(length)
+        return check;
+
+}
+
+console.log(checkEndString("nguyen dinh kha", "kha")); 
+console.log(checkEndString("nguyen dinh kha", "dinh")); 
