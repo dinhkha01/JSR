8d84e6db6b40f3037d8cf6134c5f1ae75753a1b2
dinhkha01 <149988948+dinhkha01@users.noreply.github.com>
2024-05-17 10:21:42 +0700

    ss6

--- ss6/bai3.js
+++ ss6/bai3.js
@@ -1,8 +1,13 @@
 "use strict";
 let a = 10;
 let b = 10;
+let Arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const Sum = (a, b) => {
     let c = a + b;
     return c;
 };
+const sumArr = (arr) => {
+    return arr.reduce((temp, a) => temp + a, 0);
+};
+console.log(sumArr(Arr));
 console.log(Sum(a, b));
--- ss6/bai3.ts
+++ ss6/bai3.ts
@@ -1,9 +1,15 @@
 let a: number = 10;
 let b: number = 10;
-
+let Arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const Sum = (a: number, b: number) => {
   let c: number = a + b;
   return c;
 };
+
+const sumArr = (arr: number[]) => {
+  return arr.reduce((temp: number, a: number) => temp + a, 0);
+};
+
+console.log(sumArr(Arr));
 
 console.log(Sum(a, b));
