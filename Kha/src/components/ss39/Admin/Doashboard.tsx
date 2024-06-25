7044eb9c9037a0a1d458383c63e8271fe508775b
dinhkha01 <149988948+dinhkha01@users.noreply.github.com>
2024-06-25 15:27:28 +0700

    ss41

--- Kha/src/components/ss39/Admin/Doashboard.tsx
+++ Kha/src/components/ss39/Admin/Doashboard.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import {
+  Grid3x3GapFill,
+  Calendar3,
+  Capsule,
+  Envelope,
+  Wallet2,
+  Gear,
+  QuestionCircle,
+  Bank,
+} from "react-bootstrap-icons";
 
 const Dashboard = () => {
   return (
@@ -15,15 +25,32 @@ const Dashboard = () => {
         </Navbar.Brand>
         <div className="d-flex flex-column justify-content-between h-100">
           <Nav className="flex-column">
-            <Nav.Link className="mb-2">Dashboard</Nav.Link>
-            <Nav.Link className="mb-2">Lab Test</Nav.Link>
-            <Nav.Link className="mb-2">Appointment</Nav.Link>
-            <Nav.Link className="mb-2">Message</Nav.Link>
-            <Nav.Link className="mb-2">Payment</Nav.Link>
-            <Nav.Link className="mb-2">Setting</Nav.Link>
+            <Nav.Link className="mb-2 d-flex align-items-center">
+              <Grid3x3GapFill className="me-2" /> Dashboard
+            </Nav.Link>
+            <Nav.Link className="mb-2 d-flex align-items-center">
+              <Bank className="me-2" /> Lab Test
+            </Nav.Link>
+            <Nav.Link className="mb-2 d-flex align-items-center">
+              <Calendar3 className="me-2" /> Appointment
+            </Nav.Link>
+            <Nav.Link className="mb-2 d-flex align-items-center">
+              <Capsule className="me-2" /> Medicine Order
+            </Nav.Link>
+            <Nav.Link className="mb-2 d-flex align-items-center">
+              <Envelope className="me-2" /> Message
+            </Nav.Link>
+            <Nav.Link className="mb-2 d-flex align-items-center">
+              <Wallet2 className="me-2" /> Payment
+            </Nav.Link>
+            <Nav.Link className="mb-2 d-flex align-items-center">
+              <Gear className="me-2" /> Settings
+            </Nav.Link>
           </Nav>
           <Nav className="flex-column mt-auto">
-            <Nav.Link>Help</Nav.Link>
+            <Nav.Link className="d-flex align-items-center">
+              <QuestionCircle className="me-2" /> Help
+            </Nav.Link>
           </Nav>
         </div>
       </Navbar>
